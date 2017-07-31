ae3a7e07cbda4b67d28947a4f1e4e9d9dc78592b
Noer <noerpaanakker@hotmail.com>
2017-07-31 18:20:10 +0200

    added drum functionality, favicon and styling

--- public/js/drums.js
+++ public/js/drums.js
@@ -1,4 +1,17 @@
 window.addEventListener("keydown", function(event) {
-	const audio = document.querySelector();
-	console.log(event.keyCode);
+	const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+	const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+	if(!audio) return; //stop if it's an irrelevant keycode
+	audio.currentTime = 0; //resets the playing to the start
+	audio.play();
+	key.classList.add('playing');
 });
+
+function removeTransition(event) {
+	if(event.propertyName !== "transform") return; //skip if the property isn't transform
+	this.classList.remove('playing');
+};
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
